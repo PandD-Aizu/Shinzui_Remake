a1485131322ebc1fca65ddc7d8b5f8b134c5284b
momiji1107 <yokohirahazuki0607@gmail.com>
2026-06-21 02:53:13 +0900

    プレイヤーが一定範囲内にいないとき、ランダムに徘徊する処理を実装した。
    Enemyの子オブジェクトであるcollider内にプレイヤーがいないとき、EnemyのMovementStateはWandering(徘徊)状態となる。
    Wandering状態のとき、一定時間(wanderInterval)ごとにランダムに移動先を決め、移動する。
    Playerがcollider内に入るとMovementStateはIsChasing(追従)状態になる。
    IsChasing状態のときはプレイヤーを追従する。

--- Assets/Shinzui/Src/Presentation/EnemyPresenter.cs
+++ Assets/Shinzui/Src/Presentation/EnemyPresenter.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using UnityEngine.AI;
+using Shinzui.Application.UseCases;
 
-public class EnemyPresenter : MonoBehaviour
+namespace Shinzui.Presentation
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class EnemyPresenter : MonoBehaviour
     {
-        
-    }
+        private EnemyMoveUseCase _enemyMoveUseCase;
+        private NavMeshAgent _agent;
+        private Transform _target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float _timer;                            //時間計測用タイマー
+        private readonly float _wanderInterval = 10.0f;  //徘徊目的地を更新するインターバル
+        private readonly float _wanderRadius = 10.0f;    //徘徊範囲
+
+        void Start()
+        {
+            _enemyMoveUseCase = gameObject.AddComponent<EnemyMoveUseCase>();
+            _agent = transform.parent.GetComponent<NavMeshAgent>();
+            _target = GameObject.Find("Player").GetComponent<Transform>();
+            _timer = _wanderInterval;
+        }
+
+        void Update()
+        {
+            // 敵が徘徊状態なら、一定時間間隔で移動先を決めて移動する
+            if (_enemyMoveUseCase.IsWandering)
+            {
+                _timer += Time.deltaTime;
+                if (_timer > _wanderInterval)
+                {
+                    Debug.Log("wandering: " + _agent.name);
+                    Vector3 targetPosition = RandomTarget(transform.position, _wanderRadius);
+                    _enemyMoveUseCase.SetDestination(_agent, targetPosition);
+                    _timer = 0;
+                }
+            }
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            if (_agent == null || _target == null) return;
+
+            if (other.name == "Player")
+            {
+                _enemyMoveUseCase.UpdateEnemyState(2); //敵のMovementStateをIsChasingに変更する
+                _enemyMoveUseCase.SetDestination(_agent, _target.position);
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            _enemyMoveUseCase.UpdateEnemyState(1); //敵のMovementStateをWanderingに変更する
+        }
+
+        /// <summary>
+        /// 現在位置から徘徊範囲内でランダムに移動先を決める
+        /// </summary>
+        /// <param name="origin">現在位置</param>
+        /// <param name="radius">徘徊範囲</param>
+        /// <returns></returns>
+        Vector3 RandomTarget(Vector3 origin, float radius)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * radius;
+            randomDirection += origin;
+            
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, radius, NavMesh.AllAreas);
+            return hit.position;
+        }
     }
 }
