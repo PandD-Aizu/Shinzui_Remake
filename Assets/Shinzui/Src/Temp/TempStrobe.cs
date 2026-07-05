4c65e90a5fbb0a7571329b85119d9035f3179c0f
koto-thing <gotoukenta62@gmail.com>
2026-07-05 20:18:47 +0900

    ストロボTemp作成

---
+++ Assets/Shinzui/Src/Temp/TempStrobe.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shinzui.Temp
+{
+    /// <summary>
+    /// 懐中電灯のストロボギミック実装
+    /// 左クリック長押しで、ためて、離すと一気に光る
+    /// 
+    /// </summary>
+    public class TempStrobe : MonoBehaviour
+    {
+        [SerializeField] private float maxStrobeIntensity;
+        [SerializeField] private float minStrobeIntensity;
+        [SerializeField] private float strobeInterval;
+        [SerializeField] private float intensityChangeSpeed;
+
+        private float currentIntensity = 0.0f;
+        private float currentPushTime = 0.0f; // 左クリックを押し続けている時間
+
+        public void Update()
+        {
+            if (Mouse.current.leftButton.isPressed)
+            {
+                currentPushTime += Time.deltaTime;
+            }
+            else if (currentPushTime > 0)
+            {
+                StartStrobe();
+            }
+        }
+
+        private void StartStrobe()
+        {
+            // ここで一気に、懐中電灯の光を強くして、徐々に弱くするようにする
+            
+        }
+    }
+}
