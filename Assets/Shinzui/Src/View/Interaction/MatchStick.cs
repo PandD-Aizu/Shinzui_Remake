8a3dc7242bfca7dd5a7a7bc00177a205fadef0fb
momiji1107 <yokohirahazuki0607@gmail.com>
2026-08-15 01:34:22 +0900

    アイテム定義にマッチ棒追加し、ステージ上で拾えるようにした。
    マッチ棒にカーソルを合わせ、Eキーで拾うことができる。

---
+++ Assets/Shinzui/Src/View/Interaction/MatchStick.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Shinzui.View.Interaction
+{
+    /// <summary>
+    /// Eキー押下でインベントリにアイテムを追加するテストインタラクトオブジェクト
+    /// </summary>
+    public class MatchStick : InteractableComponent
+    {
+        public override string InteractableId => "MatchStick";
+    }
+}
